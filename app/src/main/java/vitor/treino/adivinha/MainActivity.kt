4802972a81364517fae0296f3825c530821324a0
segur <seguro17@gmail.com>
2021-05-07 10:02:56 +0100

    Função do botão

--- app/src/main/java/vitor/treino/adivinha/MainActivity.kt
+++ app/src/main/java/vitor/treino/adivinha/MainActivity.kt
@@ -2,10 +2,27 @@ package vitor.treino.adivinha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private val random = Random()
+    private var numeroAdivinhar : Int = 0
+    private var jogo : Int = 0
+    private var tentativas : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        novoJogo()
     }
+
+    private fun novoJogo() {
+        numeroAdivinhar = random.nextInt(10) + 1
+        tentativas = 0
+        jogo++
+    }
+
+    fun adivinha(view: View) {}
 }
