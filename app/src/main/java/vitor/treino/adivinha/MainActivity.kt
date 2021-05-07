cc89a8fa55bd9d769c62a0ee57ba721c2ced89db
segur <seguro17@gmail.com>
2021-05-07 10:49:07 +0100

    Criação da funcao novamente

--- app/src/main/java/vitor/treino/adivinha/MainActivity.kt
+++ app/src/main/java/vitor/treino/adivinha/MainActivity.kt
@@ -51,16 +51,19 @@ class MainActivity : AppCompatActivity() {
     private fun verificaAcertou(numero: Int) {
         val textViewFeedback = findViewById<TextView>(R.id.textViewFeedback)
 
-        textViewFeedback.text  =
-            if (numero == numeroAdivinhar) {
-                getString(R.string.acertou)
-            } else if (numeroAdivinhar > numero) {
-                getString(R.string.numero_maior)
-            } else {
-                getString(R.string.numero_menor)
-            }
+        if (numero == numeroAdivinhar) {
+            textViewFeedback.text  = getString(R.string.acertou)
+            novamente()
+        } else if (numeroAdivinhar > numero) {
+            textViewFeedback.text  = getString(R.string.numero_maior)
+        } else {
+            textViewFeedback.text  = getString(R.string.numero_menor)
+        }
 
         tentativas++
         atualizaTentiva()
+    }
+
+    private fun novamente() {
     }
 }
